2b736ffc4db64dcb74a9773c8b5f632fb9633372
Mosakra <liam.edwards@live.ca>
2025-04-14 23:52:16 -0230

    This commit adds the "Remove Game" navigation link in `layout.html`

--- static/verify.js
+++ static/verify.js
@@ -5,7 +5,7 @@ addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", event => {
         let inputs = document.querySelectorAll("input");
         for( let input of inputs ) {
-            if( input.value == "") {
+            if( input.value === "") {
                 event.preventDefault()
                 alert("you need to fill this out")
                 return
